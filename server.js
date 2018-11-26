f1c9f2878c07a78b29b13dffb5e96fd6df22c29f
Glitch (healthy-transport) <none>
2018-11-26 23:47:28 +0000

    🎖🏠 Checkpoint ./package.json:2025103/947 ./server.js:2025103/426

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ var cors = require('cors');
 // require and use "multer"...
 
 var app = express();
+const multer = require('multer')
+
+let upload = multer({dest: 'uploads/'})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/fileanalyse', upload.single('form'),function (req, res, next) {
+    console.log(req.file)
   });
 
 app.get('/hello', function(req, res){
